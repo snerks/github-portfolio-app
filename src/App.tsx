8a3d4a6cd001af72b71c296101e4134f8db860eb
snerks <martin.github@outlook.com>
2025-06-14 18:01:14 +0100

    Scroll into view

--- src/App.tsx
+++ src/App.tsx
@@ -454,13 +454,15 @@ function App() {
         </div>
       </section>
 
-      {/* <script src={"/js/jquery.min.js"}></script>
+      {/*       
+      <script src={"/js/jquery.min.js"}></script>
       <script src={"/js/jquery.scrollex.min.js"}></script>
       <script src={"/js/jquery.scrolly.min.js"}></script>
       <script src={"/js/browser.min.js"}></script>
       <script src={"/js/breakpoints.min.js"}></script>
       <script src={"/js/util.js"}></script>
-      <script src={"/js/main.js"}></script> */}
+      <script src={"/js/main.js"}></script>
+      */}
     </>
   )
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,50 +1,78 @@
+import { useState } from "react";
+
 export function Header() {
+
+    const [activeSection, setActiveSection] = useState("About");
+
+    const sectionLinks = [
+        { name: "About", id: "one" },
+        { name: "Things I Can Do", id: "two" },
+        { name: "A Few Apps", id: "three" },
+        { name: "A Few DevOps Apps", id: "four" }
+    ];
+
+    const handleSectionClick = (event: any, sectionName: string) => {
+        event.preventDefault();
+
+        setActiveSection(sectionName);
+
+        // const listItemElement = event.currentTarget;
+        // const childAnchorElement = listItemElement.querySelector("a");
+        // const listItemElement = event.currentTarget;
+        const childAnchorElement = event.currentTarget;
+
+        if (childAnchorElement) {
+            // // Remove the 'active' class from all links
+            // const allLinks = document.querySelectorAll("#nav a");
+            // allLinks.forEach(link => link.classList.remove("active"));
+
+            // // Add the 'active' class to the clicked link
+            // childAnchorElement.classList.add("active");
+            const childAnchorElementHref = childAnchorElement.getAttribute("href");
+            if (childAnchorElementHref) {
+                const elementId = childAnchorElementHref.substring(1);
+                const element = document.getElementById(elementId);
+                const section = element;
+                if (section) {
+                    section.scrollIntoView({ behavior: "smooth" });
+                }
+            }
+
+        }
+    };
+
     return <section id="header">
         <header>
-            <span className="image avatar"
-            ><img
+            <span className="image avatar">
+                <img
                     src="https://avatars1.githubusercontent.com/u/7326391?s=460&v=4"
-                    alt="" /></span>
+                    alt="" />
+            </span>
             <h1 id="logo"><a href="#">snerks</a></h1>
             <p style={{ margin: "0 0 2.25em 0" }}>Full-stack software development</p>
         </header>
         <nav id="nav">
             <ul>
-                <li><a href="#one" className="active">About</a></li>
-                <li><a href="#two">Things I Can Do</a></li>
-                <li><a href="#three">A Few Apps</a></li>
-                <li><a href="#four">A Few DevOps Apps</a></li>
-                {/* <li><a href="#five">Contact</a></li> */}
+                {/* <li onClick={_ => setActiveSection("About")}><a href="#one" className={activeSection === "About" ? "active" : ""}>About</a></li>
+                <li onClick={_ => setActiveSection("Things I Can Do")}><a href="#two" className={activeSection === "Things I Can Do" ? "active" : ""}>Things I Can Do</a></li>
+                <li onClick={_ => setActiveSection("A Few Apps")}><a href="#three" className={activeSection === "A Few Apps" ? "active" : ""}>A Few Apps</a></li>
+                <li onClick={_ => setActiveSection("A Few DevOps Apps")}><a href="#four" className={activeSection === "A Few DevOps Apps" ? "active" : ""}>A Few DevOps Apps</a></li> */}
+
+                {sectionLinks.map((section) => (
+                    <li key={section.id} >
+                        <a href={`#${section.id}`} onClick={(event) => handleSectionClick(event, section.name)} className={activeSection === section.name ? "active" : ""}>
+                            {section.name}
+                        </a>
+                    </li>
+                ))}
             </ul>
         </nav>
         <footer>
             <ul className="icons">
-                {/* <li>
-          <a href="#" className="icon brands fa-twitter"
-          ><span className="label">Twitter</span></a
-          >
-        </li>
-        <li>
-          <a href="#" className="icon brands fa-facebook-f"
-          ><span className="label">Facebook</span></a
-          >
-        </li>
-        <li>
-          <a href="#" className="icon brands fa-instagram"
-          ><span className="label">Instagram</span></a
-          >
-        </li> */}
                 <li>
                     <a href="https://github.com/snerks" className="icon brands fa-github"
                     ><span className="label">Github</span></a>
                 </li>
-
-                {/* <li>
-          <a href="#" className="icon solid fa-envelope"
-          ><span className="label">Email</span></a
-          >
-        </li>  */}
-
             </ul>
         </footer>
     </section>;
